50fb0b22d7c86f731424b71c5ce4b90fc2870c44
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-13 18:25:44 -0400

    Tentative Push

--- src/MultiState_Sales_Tax.java
+++ src/MultiState_Sales_Tax.java
@@ -4,10 +4,41 @@
  */
 
 
-
+import java.util.Scanner;
 
 public class MultiState_Sales_Tax {
     public static void main(String [] arg){
+        System.out.print("What is the order amount? ");
+        Scanner amtInp = new Scanner(System.in);
+        double orderAmt = amtInp.nextInt();
+
+        System.out.print("What is the name of the state that you live in? ");
+        Scanner stateInp = new Scanner(System.in);
+        String state = stateInp.nextLine();
+        double taxrate = .05;
+
+
+        if(state.equals("Wisconsin"))
+            System.out.print("What county do you live in? ");
+            Scanner countyInp = new Scanner(System.in);
+            String county = countyInp.nextLine();
+
+            if(county.equals("Eau Claire"))
+                taxrate = .055;
+
+
+            if(county.equals("Dun"))
+                taxrate = .054;
+
+
+        if(state.equals("Illinois"))
+            taxrate = .08;
+
+        double tax = (orderAmt * taxrate);
+        double total = orderAmt + tax;
+
+        System.out.printf("The tax is $%.2f.\nThe total is $%.2f.", tax, total);
+
 
     }
 }
